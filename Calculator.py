59b51f8ad01193059b164aa9ab9fc0a398852e8a
dip parmar <dipparmar4637@gmail.com>
2024-12-24 11:25:41 +0530

    add docstring to calculator
    improve readability of code

--- Calculator.py
+++ Calculator.py
@@ -1,22 +1,60 @@
 #dipparmar(DD)calculater
 def sum(x,y):
+	"""Function To Return Addition Of Two Numbers
+
+	Args:
+		x (int): First Number
+		y (int): Second Number
+
+	Returns:
+		int: Addition Of First And Second Number
+	"""		
 	return x+y
 
 #dipparmar(DD)calculater
 def sub(x,y):
+	"""Function To Return Subtraction Of Two Numbers
+
+	Args:
+		x (int): First Number
+		y (int): Second Number
+
+	Returns:
+		int: Subtraction Of First And Second Number
+	"""	
 	return x-y
 
 #dipparmar(DD)calculater
 def  div(x,y):
+	"""Function To Return Division Of Two Numbers
+
+	Args:
+		x (int): First Number
+		y (int): Second Number
+
+	Returns:
+		int: Division Of First And Second Number
+	"""	
 	return x/y
 
 #dipparmar(DD)calculater
 def mul(x,y):
+	"""Function To Return Multiplication Of Two Numbers
+
+	Args:
+		x (int): First Number
+		y (int): Second Number
+
+	Returns:
+		int: Multiplication Of First And Second Number
+	"""	
 	return x*y
 
 #dipparmar(DD)calculater
 print("welcome to dip parmar calculater.")
 def calculater():
+	"""TO Print and input Calculator representation
+	"""	
 	enter=1
 	sign="dip"
 	while(enter!=0):
@@ -64,4 +102,3 @@ calculater()
 
 print("thanks for use dip parmar (D D) calculater.")
 #dipparmar(DD)calculater
-		
